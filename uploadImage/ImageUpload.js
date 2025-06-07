8a1b8f61b1183f14098e78cacd9b681fd3009f40
Abhishek-K-Vishvakarma <gitrepo101@gmail.com>
2025-06-07 13:25:36 +0530

    Commit to all changes my project

--- uploadImage/ImageUpload.js
+++ uploadImage/ImageUpload.js
@@ -2,13 +2,11 @@ const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 
-// Ensure "uploads/" folder exists
 const uploadDir = "./uploads/";
 if (!fs.existsSync(uploadDir)){
   fs.mkdirSync(uploadDir);
 }
 
-// Configure Multer Storage
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, uploadDir);
